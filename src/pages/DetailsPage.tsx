d15ae7981e4a6677998b13dd49436d6c4a61f822
Alibek <musicchannel0p@gmail.com>
2023-04-16 13:53:09 +0600

    details

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { products } from "../helpers/variables";
 
+interface Product {
+  id: number;
+  image: any;
+  description: string;
+  title: string;
+  price: number;
+  characters: string;
+}
+
 const DetailsPage = () => {
-  return (
-    <div>
-      {products.map((product) => (
-        <p key={product.id}>
-          {product.charackters}
-          {product.description}
-          {product.image}
-          {product.price}
-          {product.title}
-        </p>
-      ))}
-    </div>
-  );
+  const { id } = useParams<{ id: string }>();
+  const [oneProduct, setOneProduct] = useState<Product>();
+
+  useEffect(() => {
+    const filteredProducts: Product[] = products.filter(
+      (product) => product.id === Number(id)
+    );
+    setOneProduct(filteredProducts[0]);
+  }, [id]);
+
+  return <div></div>;
 };
 
 export default DetailsPage;
